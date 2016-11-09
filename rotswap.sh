5f4edac9b290eea24da09c9260fbe8293cc694b8
pbnelson <peter@peternelson.com>
2016-11-09 15:35:50 -0600

    Use " instead of ' in device name

--- rotswap.sh
+++ rotswap.sh
@@ -20,15 +20,15 @@ TOUCHPAD='ITE Tech. Inc. ITE Device(8595) Touchpad'
 if [ -f $CHKFILE ]; then
   rm $CHKFILE
   xrandr -o normal &
-  xinput --set-prop '$TOUCHSCREEN' 'Evdev Axes Swap' 0 &
-  xinput --set-prop '$TOUCHSCREEN' 'Evdev Axis Inversion' 0 0 &
-  xinput --set-prop '$TOUCHPAD' 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1 &
+  xinput --set-prop "$TOUCHSCREEN" 'Evdev Axes Swap' 0 &
+  xinput --set-prop "$TOUCHSCREEN" 'Evdev Axis Inversion' 0 0 &
+  xinput --set-prop "$TOUCHPAD" 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1 &
   gsettings set com.canonical.Unity.Launcher launcher-position Left &
 else
   touch $CHKFILE
   xrandr -o right &
-  xinput --set-prop '$TOUCHSCREEN' 'Evdev Axes Swap' 1 &
-  xinput --set-prop '$TOUCHSCREEN' 'Evdev Axis Inversion' 0 1 &
-  xinput --set-prop '$TOUCHPAD'  'Coordinate Transformation Matrix' 0 1 0 -1 0 1 0 0 1 &
+  xinput --set-prop "$TOUCHSCREEN" 'Evdev Axes Swap' 1 &
+  xinput --set-prop "$TOUCHSCREEN" 'Evdev Axis Inversion' 0 1 &
+  xinput --set-prop "$TOUCHPAD"  'Coordinate Transformation Matrix' 0 1 0 -1 0 1 0 0 1 &
   gsettings set com.canonical.Unity.Launcher launcher-position Bottom &
 fi
